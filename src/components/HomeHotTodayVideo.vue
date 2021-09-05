b0f2be1a57e574bdeae8b0edc89f4f8caae46c08
guoguihong <guoguihong@qq.com>
2021-09-06 04:20:47 +0800

    首页内容封装成组件

---
+++ src/components/HomeHotTodayVideo.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="hot-today-video-wrap">
+    <div class="inner">
+      <h1 class="title">今日热门视频菜谱</h1>
+      <div class="video-list">
+        <div class="item" v-for="item in hotTodayVideoList" :key="item.id">
+          <div class="mask"></div>
+          <img class="cover" :src="item.coverUrl" />
+          <i class="play-btn"></i>
+          <div class="info">
+            <div class="t">{{item.title}}</div>
+            <div class="row">
+              <div class="avatar"
+                    :style="{backgroundImage: `url(${item.userAvatar})`}"></div>
+              <div class="username">{{item.username}}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    hotTodayVideoList: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.hot-today-video-wrap {
+  background: #efeeec;
+
+  .inner {
+    width: 1200px;
+    margin: 0 auto;
+    padding: 14px 0 40px;
+
+    .title {
+      color: #222;
+      font-size: 24px;
+      font-weight: 600;
+      padding: 20px 0;
+    }
+
+    .video-list {
+      display: flex;
+      flex-wrap: wrap;
+
+      .item {
+        position: relative;
+        width: 285px;
+        height: 526px;
+        margin-right: 20px;
+        border-radius: 12px;
+        overflow: hidden;
+        cursor: pointer;
+
+        &:nth-child(4n) {
+          margin-right: 0;
+        }
+
+        .mask {
+          position: absolute;
+          left: 0;
+          right: 0;
+          top: 0;
+          bottom: 0;
+          background: rgba(0,0,0,.5);
+          z-index: 1;
+        }
+
+        .cover {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          filter: blur(8px);
+          opacity: .5;
+        }
+
+        .play-btn {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          display: block;
+          width: 100px;
+          height: 100px;
+          background: url(../assets/images/sprite_03.png) 0px -20px no-repeat;
+          background-size: 100px 250px;
+          z-index: 10;
+          opacity: .8;
+          transform: all ease .3s;
+
+          &:hover {
+            opacity: 1;
+          }
+        }
+
+        .info {
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 100%;
+          padding: 0 20px;
+          color: #fff;
+          z-index: 5;
+          box-sizing: border-box;
+
+          .t {
+            font-size: 22px;
+            font-weight: 700;
+          }
+
+          .row {
+            display: flex;
+            align-items: center;
+            padding: 16px 0 20px;
+
+            .avatar {
+              width: 32px;
+              height: 32px;
+              border-radius: 50%;
+            }
+
+            .username {
+              font-size: 14px;
+              font-weight: 600;
+              padding-left: 10px;
+            }
+          }
+        }
+      }
+    }
+
+  }
+
+}
+</style>
---
+++ src/components/HomeRecommentArea.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="home-recomment-area">
+    <div class="list-wrap" v-for="item in recommentList" :key="item.id">
+      <div class="title">{{item.columnTitle}}</div>
+      <div class="list">
+        <div class="item" v-for="item2 in item.list" :key="item2.id">
+          <div
+            class="cover"
+            :style="{backgroundImage: `url(${item2.coverUrl})`}"
+          >
+            <i class="video-icon" v-if="item2.isVideo"></i>
+            <div class="author-info">
+              <div class="author-avatar" :style="{backgroundImage: `url(${item2.authorAvatar})`}"></div>
+              <strong class="author-name ellipsis-l1">{{item2.authorName}}</strong>
+            </div>
+          </div>
+          <div class="info">
+            <strong class="t ellipsis-l1">{{item2.title}}</strong>
+            <span class="desc ellipsis-l1">{{item2.desc}}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    recommentList: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home-recomment-area {
+  width: 1200px;
+  margin: 0 auto;
+
+  .list-wrap {
+    margin-top: 20px;
+
+    .title {
+      color: #222;
+      font-size: 24px;
+      font-weight: 600;
+      padding: 20px 0;
+    }
+
+    .list {
+      .item {
+        display: inline-block;
+        width: 224px;
+        height: 360px;
+        background: #fff;
+        border-radius: 12px;
+        overflow: hidden;
+        margin: 0 20px 20px 0;
+
+        &:nth-child(5n) {
+          margin-right: 0;
+        }
+
+        .cover {
+          position: relative;
+          background-repeat: no-repeat;
+          background-size: cover;
+          background-position: center;
+          height: 280px;
+          cursor: pointer;
+          overflow: hidden;
+
+          &:hover {
+            .author-info {
+              bottom: 0;
+            }
+          }
+
+          .video-icon {
+            position: absolute;
+            top: 15px;
+            right: 15px;
+            width: 30px;
+            height: 30px;
+            background: url(../assets/images/sprite_04.png) 0px 0px no-repeat;
+            background-size: 30px 60px;
+          }
+
+          .author-info {
+            position: absolute;
+            bottom: -60px;
+            width: 100%;
+            height: 60px;
+            background: rgba(0,0,0,.5);
+            display: flex;
+            align-items: center;
+            padding: 0 20px;
+            box-sizing: border-box;
+            transition: bottom ease .3s;
+
+            &:hover {
+              .author-name {
+                color: #f64d36;
+              }
+            }
+
+            .author-avatar {
+              flex-shrink: 0;
+              background-size: cover;
+              background-position: center;
+              background-repeat: no-repeat;
+              width: 36px;
+              height: 36px;
+              border-radius: 50%;
+              margin-right: 16px;
+            }
+
+            .author-name {
+              color: #fff;
+              font-size: 14px;
+            }
+          }
+        }
+
+        .info {
+          cursor: pointer;
+
+          &:hover {
+            .t {
+              color: #f64d36;
+            }
+          }
+
+          .t {
+            display: block;
+            color: #222;
+            font-size: 16px;
+            font-weight: 600;
+            margin: 12px 20px 0;
+            line-height: 28px;
+          }
+
+          .desc {
+            display: block;
+            position: relative;
+            color: #999;
+            font-size: 14px;
+            padding: 0 20px 0 44px;
+            line-height: 33px;
+            box-sizing: border-box;
+
+            &::before {
+              content: '';
+              position: absolute;
+              left: 20px;
+              top: 8px;
+              width: 16px;
+              height: 16px;
+              background: url(../assets/images/sprite_01.png) 0px -60px no-repeat;
+              background-size: 30px 150px;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
